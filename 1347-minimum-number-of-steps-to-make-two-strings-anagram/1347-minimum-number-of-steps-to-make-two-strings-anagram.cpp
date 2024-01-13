34cb9a85fa71b35d30f8687c611eab5ba04af866
Hritik422 <106082040+Hritik422@users.noreply.github.com>
2024-01-13 16:36:13 +0530

    Time: 70 ms (49.19%), Space: 17.1 MB (36.03%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        map<char,int>mp,mp1;
+        unordered_map<char,int>mp,mp1;
         int remain=0,over=0;
         for(auto it:s)mp[it]++;
         for(auto it:t)mp1[it]++;
