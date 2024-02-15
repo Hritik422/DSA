15dd4d945b4de704cd7b8f4909b6dba2060c3497
Hritik422 <106082040+Hritik422@users.noreply.github.com>
2024-02-16 00:20:24 +0530

    Time: 139 ms (77.19%), Space: 83.3 MB (35.28%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
@@ -3,26 +3,16 @@ public:
     long long largestPerimeter(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         long long sum=nums[0]+nums[1],ans=-1,n=nums.size(),last=-1;  
-      //  for(int i=2;i<n;i++){
-      //      if(sum<=nums[i]){
-      //          if(last==i-1)ans=max(ans,sum);
-      //          sum+=nums[i];
-      //      }
-      //      else{
-      //          sum+=nums[i];
-      //          last=i;
-      //      }
-      // }
         for(int i=2;i<n;i++){
             if(sum>nums[i]){
                 last=i;
+                ans=sum+nums[i];
             }
             sum+=nums[i];
         }
-        long long tot=0;
-        for(int i=0;i<=last;i++)tot+=nums[i];
+        
         if(last==-1)return last;
-        return tot;
+        return ans;
         
     }
 };
