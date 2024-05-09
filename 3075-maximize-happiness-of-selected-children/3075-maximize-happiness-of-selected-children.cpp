aea09c721670b7e23dbde3fce5202ed1b437b122
Hritik422 <106082040+Hritik422@users.noreply.github.com>
2024-05-10 00:49:59 +0530

    Time: 226 ms (13.44%), Space: 107.1 MB (89.28%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        sort(happiness.begin(),happiness.end(),greater<int>());
+        partial_sort(happiness.begin(), happiness.begin()+k, happiness.end(), greater<int>());
         long long prev=0,ans=0;
         for(int i=0;i<k;i++){
             happiness[i]-=prev;
